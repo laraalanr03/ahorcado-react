6dc9d701671ee3f3eee9e6960f471f85ee06c6c3
laraalanr03 <laraalanr03@gmail.com>
2026-07-28 15:05:28 -0600

    avances del juego del ahorcado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import { HangImage } from './components/HangImage';
+import { letters } from './helpers/letters';
 import './App.css'
+
+
 
 function App() {
   return (
 
-    <div className = "App">
+    <div className="App" translate="no">
 
-      <h1> Hola mundo </h1>
+     {/* Imagenes */}
+    <HangImage imageNumber= {9}  />
+    
+
+    {/* Palabra oculta */}
+
+    <h3>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</h3>   
+
+    {/* Contador de intentos */}
+
+    <h3> Intentos restantes: 0 </h3>   
+
+    {/* Botones letras */}
+   
+    {
+      
+      letters.map(( letter ) => (
+        <button 
+          key={letter}> 
+            {letter} 
+          </button>
+
+      ))
+
+   }
+    
 
     </div>
 
@@ -16,3 +45,4 @@ function App() {
 }
 
 export default App
+
