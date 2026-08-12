53e13535c99a17d788d4cd0c60dfb617261310dc
laraalanr03 <laraalanr03@gmail.com>
2026-08-12 15:25:47 -0600

    Avance ahorcado juego Miercoles 08 de agosto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import { HangImage } from './components/HangImage';
 import { letters } from './helpers/letters';
+import { useState } from 'react';
+
 import './App.css'
 
 
 
 function App() {
+  
+  const [word] = useState ('COMPUTADORA');
+  const [hiddenWord] = useState ("_ ".repeat(word.length));
+  const [attempts, setAttempts] = useState (0);
+  
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+    setAttempts(Math.min(attempts + 1, 9));
+
+  }
+
+  
   return (
 
     <div className="App" translate="no">
 
      {/* Imagenes */}
-    <HangImage imageNumber= {9}  />
+    <HangImage imageNumber= {attempts}  />
     
 
     {/* Palabra oculta */}
 
-    <h3>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</h3>   
+    <h3> {hiddenWord} </h3>   
 
     {/* Contador de intentos */}
 
-    <h3> Intentos restantes: 0 </h3>   
+    <h3> Intentos restantes: {attempts} </h3>   
 
     {/* Botones letras */}
    
@@ -27,6 +41,7 @@ function App() {
       
       letters.map(( letter ) => (
         <button 
+        onClick = { () => checkLetter (letter)}
           key={letter}> 
             {letter} 
           </button>
